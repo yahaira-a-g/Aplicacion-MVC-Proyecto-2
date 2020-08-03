b90476bc154a3ffcf4e0ed9249bc3638a8da72e5
Yahaira Adama <yahairaadamagalarza@gmail.com>
2020-08-03 12:54:00 -0500

    Clase Producto.java Listooo

--- src/java/Modelo/Producto.java
+++ src/java/Modelo/Producto.java
@@ -1,14 +1,65 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Modelo;
 
-/**
- *
- * @author Usuario
- */
 public class Producto {
+    private int idproducto;
+    private String codigo;
+    private String nombre;
+    private String cantidad;
+    private String costo;
+    private String estado;
+    
+    //Constructor Vacio
+    public Producto(){ 
+    }
+    public Producto(String codigo, String nombre, String cantidad, String costo,  String estado){ 
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.costo = costo;
+        this.estado = estado;
+    }
+
+    public int getIdproducto() {
+        return idproducto;
+    }
+    public void setIdproducto(int idproducto) {
+        this.idproducto = idproducto;
+    }
+    
+    public String getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCantidad() {
+        return cantidad;
+    }
+    public void setCantidad(String apellidos) {
+        this.cantidad = cantidad;
+    }
+
+    public String getCosto() {
+        return costo;
+    }
+    public void setCosto(String dni) {
+        this.costo = costo;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+   
     
 }
